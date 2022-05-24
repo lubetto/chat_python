e319806ad89a40f207d6b0c9ac7bef1f68cedb29
lubetto <lubetto@gmail.com>
2022-05-23 21:02:34 -0500

    IP server

--- Cliente.py
+++ Cliente.py
@@ -1,51 +1,51 @@
-import socket
-import errno
-import sys
+import socket #  manejo de conexiones
+import errno # manejo de errores
+import sys # manejo de sistema
 
-HEAD_LENGHT = 10
-IP = input("IP del servidor: ")
-PORT = int(input("Puerto de la conexión: "))
-my_username = input("Escriba su nombre: ")
+HEAD_LENGHT = 10 # longitud maxima del mensaje
+IP = input("IP del servidor: ") # direccion IP del servidor
+PORT = int(input("Puerto de la conexión: ")) # puerto de conexion del servidor
+my_username = input("Escriba su nombre: ") # nombre del cliente
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((IP, PORT))
-client_socket.setblocking(False)
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # creacion del socket tipo internet
+client_socket.connect((IP, PORT)) # conexion del socket con la direccion IP y el puerto
+client_socket.setblocking(False) # deshabilita el bloqueo de la conexion
 
-username = my_username.encode('utf-8')
-username_header = f"{len(username):<{HEAD_LENGHT}}".encode('utf-8')
-client_socket.send(username_header + username)
+username = my_username.encode('utf-8') # codifica el nombre del cliente
+username_header = f"{len(username):<{HEAD_LENGHT}}".encode('utf-8') # codifica la cabecera del nombre del cliente
+client_socket.send(username_header + username) # envia el nombre del cliente
 
-while True:
-    message = input(f"{my_username} > ")
-    if message:
-        salir = message
-        message = message.encode('utf-8')
-        message_header = f"{len(message):<{HEAD_LENGHT}}".encode('utf-8')
-        client_socket.send(message_header + message)
-        if salir == "chao":
+while True: # ciclo infinito
+    message = input(f"{my_username} > ") # ingresa el nomnbre del cliente
+    if message: # si hay mensaje
+        salir = message #guarda mensaje en salir
+        message = message.encode('utf-8') # codifica el mensaje
+        message_header = f"{len(message):<{HEAD_LENGHT}}".encode('utf-8') # codifica la cabecera del mensaje
+        client_socket.send(message_header + message) # envia el mensaje
+        if salir == "chao": # si el mensaje es chao
                 print("Desconexión del servidor")
-                sys.exit()
+                sys.exit() # cierra el programa
 
-    try:
+    try: # intenta
         while True:
             #ciclo infinito para recibir mensajes
-            username_header = client_socket.recv(HEAD_LENGHT)
-            if not len(username_header):
+            username_header = client_socket.recv(HEAD_LENGHT) # recibe la cabecera del nombre del cliente
+            if not len(username_header): # si no hay cabecera
                 print("Servidor desconectado")
-                sys.exit()
-            username_length = int(username_header.decode('utf-8').strip())
-            username = client_socket.recv(username_length).decode('utf-8')
-            message_header = client_socket.recv(HEAD_LENGHT)
-            message_length = int(message_header.decode('utf-8').strip())
-            message = client_socket.recv(message_length).decode('utf-8')
-            print(f"{username} > {message}")
+                sys.exit() # cierra el programa
+            username_length = int(username_header.decode('utf-8').strip()) # decodifica la cabecera del nombre del cliente
+            username = client_socket.recv(username_length).decode('utf-8') # recibe el nombre del cliente
+            message_header = client_socket.recv(HEAD_LENGHT) # recibe la cabecera del mensaje
+            message_length = int(message_header.decode('utf-8').strip()) # decodifica la cabecera del mensaje y la convierte en entero
+            message = client_socket.recv(message_length).decode('utf-8') # recibe el mensaje
+            print(f"{username} > {message}") # imprime el mensaje del cliente
 
-    except IOError as e:
-        if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
-            print('Error de lectura', str(e))
-            sys.exit()
-        continue
+    except IOError as e: # manejo de errores
+        if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK: # si el error no es de tipo EAGAIN o EWOULDBLOCK
+            print('Error de lectura', str(e)) # imprime el error
+            sys.exit() # cierra el programa
+        continue # continua el ciclo
 
-    except Exception as e:
-        print('Error General', str(e))
-        pass
+    except Exception as e: # manejo de errores
+        print('Error General', str(e)) # imprime el error
+        pass # continua el ciclo
--- Server.py
+++ Server.py
@@ -1,52 +1,52 @@
-import socket
-import select
+import socket #  manejo de conexiones
+import select # manejo I/O multiple
 
-HEADER_LENGTH = 10
-IP = "127.0.0.1"
-PORT = 8080
+HEADER_LENGTH = 10 # longitud maxima del mensaje
+IP = "127.0.0.1" # direccion IP del servidor
+PORT = 8080 # puerto de conexion del servidor
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # creacion del socket tipo internet
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # reutilizacion de la direccion IP y el puerto para permitir mas conexiones
 
-server_socket.bind((IP, PORT))
-server_socket.listen()
+server_socket.bind((IP, PORT)) # conexion del socket con la direccion IP y el puerto
+server_socket.listen() # escucha de conexiones
 print("Servidor Iniciado y contestado OK")
 print("Esperando conexiones...")
-socket_list = [server_socket]
-clientes={}
+socket_list = [server_socket] # lista de sockets a escuchar
+clientes={} # diccionario de clientes
 
-def receive_message(client_socket):
+def receive_message(client_socket): # funcion para recibir mensajes
     try:
-        message_header = client_socket.recv(HEADER_LENGTH)
-        if not len(message_header):
-            return False
-        message_length = int(message_header.decode('utf-8').strip())
-        return {"header": message_header, "data": client_socket.recv(message_length)}
+        message_header = client_socket.recv(HEADER_LENGTH) # recibe el mensaje del cliente
+        if not len(message_header): # si no hay mensaje
+            return False # termina la funcion
+        message_length = int(message_header.decode('utf-8').strip()) # convierte la cabecera del mensaje a entero
+        return {"header": message_header, "data": client_socket.recv(message_length)} # retorna el mensaje
     except:
         return False
 
-while True:
-    read_sockets, _, exception_sockets = select.select(socket_list, [], socket_list)
-    for notified_socket in read_sockets:
-        if notified_socket == server_socket:
-            client_socket, client_address = server_socket.accept()
-            user = receive_message(client_socket)
-            if user is False:
-                continue
-            socket_list.append(client_socket)
-            clientes[client_socket] = user
+while True: # ciclo infinito
+    read_sockets, _, exception_sockets = select.select(socket_list, [], socket_list) # selecciona los sockets a leer
+    for notified_socket in read_sockets: # recorre los sockets a leer
+        if notified_socket == server_socket: # si el socket es el servidor
+            client_socket, client_address = server_socket.accept() # acepta la conexion del cliente
+            user = receive_message(client_socket) # recibe el mensaje del cliente
+            if user is False: # si no hay mensaje
+                continue # continua el ciclo
+            socket_list.append(client_socket) # agrega el socket a la lista de sockets a escuchar
+            clientes[client_socket] = user # agrega el cliente a la lista de clientes
             print(f"Usuario: {clientes[client_socket]['data'].decode('utf-8')} conectado")
-        else:
-            message = receive_message(notified_socket)
-            if message is False:
-                print(f"Usuario: {clientes[notified_socket]['data'].decode('utf-8')} abandonó")
-                socket_list.remove(notified_socket)
-                del clientes[notified_socket]
-                continue
-            user = clientes[notified_socket]
-            for client_socket in clientes:
-                if client_socket != notified_socket:
-                    client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
-    for notified_socket in exception_sockets:
-        socket_list.remove(notified_socket)
-        del clientes[notified_socket]
+        else: # si el socket es un cliente
+            message = receive_message(notified_socket) # recibe el mensaje del cliente
+            if message is False: # si no hay mensaje
+                print(f"Usuario: {clientes[notified_socket]['data'].decode('utf-8')} abandonó") # imprime que el cliente abandono
+                socket_list.remove(notified_socket) # remueve el socket de la lista de sockets a escuchar
+                del clientes[notified_socket] # remueve el cliente de la lista de clientes
+                continue # continua el ciclo
+            user = clientes[notified_socket] # obtiene el cliente
+            for client_socket in clientes: # recorre los clientes
+                if client_socket != notified_socket: # si el cliente no es el mismo que el que notifico
+                    client_socket.send(user['header'] + user['data'] + message['header'] + message['data']) # envia el mensaje al cliente
+    for notified_socket in exception_sockets: # recorre los sockets que generaron excepciones
+        socket_list.remove(notified_socket)# remueve el socket de la lista de sockets a escuchar
+        del clientes[notified_socket]# remueve el cliente de la lista de clientes
